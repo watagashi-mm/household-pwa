e78bee995f31d4e361d13c9d3eb349a81c857dbc
MM <MM>
2026-02-08 20:08:50 +0900

    [mod]マスタデータ調整

--- src/lib/constants/masters.ts
+++ src/lib/constants/masters.ts
@@ -6,23 +6,42 @@ export const BOP_MASTER = [
 export const CATEGORY_MASTER: Record<number, { code: number, name: string }[]> = {
     1: [ // 収入
         { code: 1, name: "給与" },
-        { code: 2, name: "その他収入" }
+        { code: 2, name: "立替" },
+        { code: 99, name: "その他" }
     ],
     2: [ // 支出
-        { code: 1, name: "食費" },
-        { code: 2, name: "交通費" },
-        { code: 3, name: "日用品" }
+        { code: 1, name: "交通費" },
+        { code: 2, name: "交際費" },
+        { code: 3, name: "個人年金" },
+        { code: 4, name: "医療費" },
+        { code: 5, name: "奨学金返済" },
+        { code: 6, name: "娯楽" },
+        { code: 7, name: "携帯電話" },
+        { code: 8, name: "日用品" },
+        { code: 9, name: "生命保険" },
+        { code: 10, name: "立替" },
+        { code: 11, name: "食費" },
+        { code: 12, name: "住居費" },
+        { code: 13, name: "衣類" },
+        { code: 14, name: "消耗品" },
+        { code: 15, name: "積立NISA" },
+        { code: 16, name: "ふるさと納税" },
+        { code: 99, name: "その他" }
     ]
 };
 
 export const PAYMENT_MASTER: Record<number, { code: number, name: string }[]> = {
     1: [ // 収入
-        { code: 1, name: "振込" }
+        { code: 1, name: "振込（みずほ）" }
     ],
     2: [ // 支出
         { code: 1, name: "現金" },
-        { code: 2, name: "クレジットカード" },
-        { code: 3, name: "電子マネー" }
+        { code: 2, name: "Suica" },
+        { code: 3, name: "UC" },
+        { code: 4, name: "引き落とし（みずほ）" },
+        { code: 5, name: "デビッド（SBI）" },
+        { code: 7, name: "Amazon" },
+        { code: 8, name: "三井住友(CL)" }
     ]
 };
 
